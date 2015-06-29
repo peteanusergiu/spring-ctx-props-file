d27576f5304a0ad9adbf60c4d892d3f03522fb3a
sergiu petean <peteanusergiu@gmail.com>
2015-06-29 15:34:08 +0300

    More options added for gradle's language on system parameters migration (build JVM 2 test JVM)

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,6 +6,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @ContextConfiguration(locations = "classpath*:/app-context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -14,12 +15,46 @@ public class AppTest {
     @Value("#{appProperties['prop.example']}")
     private String exampleProperty;
 
+    @Value("#{systemProperties['gradleProp']?: 'gradlePropDefault'}")
+    private String systemProperty;
+
     @Autowired
     private ValueHolder valueHolder;
 
     @Test
-    public void testFooVal(){
-        assertEquals("bar", System.getProperty("foo"));
+    public void testAllSystemProperties(){
+        assertNotNull(System.getProperties());
+//        System.out.println(System.getProperties());
+    }
+    
+    @Test
+    public void testSystemVarSystemProperties(){
+    	assertEquals("SYSTEM_VAR_systemProperties_VALUE", System.getProperty("SYSTEM_VAR_systemProperties"));
+    }
+
+    @Test
+    public void testSystemVarSystemPropertiesMapValue(){
+    	assertEquals("SYSTEM_VAR_systemProperties_map_VALUE", System.getProperty("SYSTEM_VAR_systemProperties_map_value"));
+    }
+
+    @Test
+    public void testSystemVarSystemPropertiesMapSysProp(){
+    	assertEquals("SYSTEM_VAR_systemProperties_map_VALUE", System.getProperty("SYSTEM_VAR_systemProperties_map_sysProp"));
+    }
+    
+    @Test
+    public void testSystemVarSystemProperty(){
+    	assertEquals("SYSTEM_VAR_systemProperty_VALUE", System.getProperty("SYSTEM_VAR_systemProperty"));
+    }
+
+    @Test
+    public void testSystemVarSystemPropertyFromGradleProp(){
+    	assertEquals("gradlePropValue", System.getProperty("gradleProp"));
+    }
+
+    @Test
+    public void testSystemVarFromSystemProperty(){
+    	assertEquals("gradlePropValue", systemProperty);
     }
 
     @Test
